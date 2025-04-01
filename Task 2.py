c2df4acbc3624ea809de34d214d85cf6238541e7
Maryna Kosse <mkosse@kse.org.ua>
2025-04-01 18:14:45 +0300

    Updated step-size search

--- Task 2.py
+++ Task 2.py
@@ -1,5 +1,6 @@
 import matplotlib.pyplot as plt
 import numpy as np
+from scipy.optimize import minimize_scalar
 
 
 def calculate_objective(x):
@@ -12,27 +13,21 @@ def compute_gradient(x):
 
 def update_hessian(h, diff, delta_g):
     diff = diff[:, np.newaxis]
-    return h + (diff @ diff.transpose()) / (delta_g.transpose() @ diff)
+    return h + ((diff @ diff.transpose()) / (delta_g.transpose() @ diff))
+
+# def update_hessian(h, diff, delta_x):
+#     diff = diff[:, np.newaxis]
+#     return h + ((diff @ diff.transpose()) / (diff.transpose() @ delta_x))
 
 
-# def calculate_step(x, direction):
-#     options = np.linspace(start=0, stop=1, num=500)
-#     function_values = np.array([])
-#     for a in options:
-#         function_values = np.append(function_values, calculate_objective(x + a * direction))
-#         # print(calculate_objective(x + a * direction))
-#     # print()
-#     return np.argmin(function_values) / 500
+def f(a, x, y, d, c):
+    return 10*(y + a*c - (x + a*d)**2)**2 + (1 - x - a*d)**2
 
 
 def calculate_step(x, direction):
-    options = np.linspace(start=0, stop=100, num=100000)
-    function_values = np.array([])
-    for a in options:
-        function_values = np.append(function_values, calculate_objective(x + a * direction))
-        # print(calculate_objective(x + a * direction))
-    # print(np.argmin(function_values))
-    return np.argmin(function_values) / 1000
+    fun = lambda a: f(a, x[0], x[1], direction[0], direction[1])
+    # print(minimize_scalar(fun, bounds=[0, 100], method='bounded').x)
+    return minimize_scalar(fun, bounds=[0, 100], method='bounded').x
 
 
 def quasi_newton_minimizer(x0, end_condition):
@@ -41,20 +36,22 @@ def quasi_newton_minimizer(x0, end_condition):
     hessians = np.array([])
 
     hessian = np.identity(2)
-    for i in range(100):
-        if i % 1000 == 0:
-            print(f"Iteration {i}: x = {x0}")
+    for i in range(10000):
         gradient = compute_gradient(x0)
-        direction = -(hessian @ gradient)
-        x1 = x0 + direction * calculate_step(x0, direction) * 1.35
+        direction = np.linalg.solve(hessian, -gradient)
+        x1 = x0 + direction * calculate_step(x0, direction)
         function_value = calculate_objective(x0)
         delta_gradient = compute_gradient(x1) - gradient
-        if np.linalg.norm(delta_gradient) <= end_condition:
+        if abs(function_value - calculate_objective(x1)) <= end_condition:
             break
         else:
             delta_x = x1 - x0
             difference = delta_x - hessian @ delta_gradient
             hessian = update_hessian(hessian, difference, delta_gradient)
+
+            # delta_x = x1 - x0
+            # difference = delta_gradient - hessian @ delta_x
+            # hessian = update_hessian(hessian, difference, delta_x)
 
             x0 = x1
             results = np.append(results, function_value)
@@ -64,19 +61,19 @@ def quasi_newton_minimizer(x0, end_condition):
     return x1, i, results, directions, hessians
 
 
-start1 = np.array([2, 4]).transpose()
-start2 = np.array([-2, 10]).transpose()
+start1 = np.array([2, 4])
+start2 = np.array([-2, 10])
 epsilon = 0.000001
 
 
-# descent1 = quasi_newton_minimizer(start1, epsilon)
-# print(f"Found x* = {descent1[0]} starting from {start1} in {descent1[1]} iterations")
-# calculations1 = descent1[2]
+descent1 = quasi_newton_minimizer(start1, epsilon)
+print(f"Found x* = {descent1[0]} starting from {start1} in {descent1[1]} iterations")
+calculations1 = descent1[2]
 
 descent2 = quasi_newton_minimizer(start2, epsilon)
 print(f"Found x* = {descent2[0]} starting from {start2} in {descent2[1]} iterations")
 calculations2 = descent2[2]
 
-# plt.plot(calculations1)
+plt.plot(calculations1)
 plt.plot(calculations2)
 plt.show()
